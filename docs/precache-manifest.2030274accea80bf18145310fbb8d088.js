63bc865b330854cd96b7aa7dcb0bb26d6ca065c3
Daniel Ayala <djdanielayalads99@gmail.com>
2020-07-08 18:29:06 -0500

    docs2 add

---
+++ docs/precache-manifest.2030274accea80bf18145310fbb8d088.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50aa283a1d6d87dde3eb3a0d3e51aacc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfe136ebdaf7cd95acd3",
+    "url": "/static/css/main.360f3ab7.chunk.css"
+  },
+  {
+    "revision": "dcb84e16ec500957bf9f",
+    "url": "/static/js/2.e930342a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e930342a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfe136ebdaf7cd95acd3",
+    "url": "/static/js/main.1ed0d048.chunk.js"
+  },
+  {
+    "revision": "a70e4ae6bdd791779146",
+    "url": "/static/js/runtime-main.1e57df78.js"
+  }
+]);
